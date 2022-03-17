081f09a7c03802cc8f696a920d2e56b4dcb5aca4
Jesus Serato <linaresjesus409@gmail.com>
2022-03-17 13:56:57 -0300

    Loading

---
+++ script.js
@@ -0,0 +1,9 @@
+/*funcion flecha */
+
+window.onload = () => {
+    let loading = document.getElementById('lottie-player-wrapper')
+    setTimeout(() => {
+        loading.style.display = 'none';
+    }, 2200)
+    /*tiempo que quieras que persista la imagen */
+};
